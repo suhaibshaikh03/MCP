d74888a7ceeec9ad8c37b1e4db4e12a08e4c4bb8
Suhaib Shaikh <suhaibshaikh2003@gmail.com>
2025-07-17 21:52:13 +0500

    add tools

--- mcp-hello/main.py
+++ mcp-hello/main.py
@@ -1,5 +1,14 @@
 from mcp.server.fastmcp import FastMCP
 mcp = FastMCP(name="hello-mcp",stateless_http=True)
+
+@mcp.tool()
+def search_online(query:str):
+    return f"search online for {query}"
+
+@mcp.tool()
+async def get_weather(city:str):
+    """Gets weather for a city"""
+    return f"Weather in {city} is sunny"
+
 mcp_app = mcp.streamable_http_app()
 
-#sample method in python
